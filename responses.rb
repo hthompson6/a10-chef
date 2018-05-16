a9e47526dee869cc2c57f23ba6c7d2cd24d567cf
Hunter Thompson <thompson.grey.hunter@gmail.com>
2018-05-16 22:26:17 +0000

    Quick save before regex

--- responses.rb
+++ responses.rb
@@ -14,176 +14,215 @@
 
 require_relative 'errors'
 
-RESPONSE_CODES = {
-    33619969: {
-        '*': {
-            '*': AE::InUse
-        }
-    },
-    67371011: {
-        '*': {
-            '*': AE::Exists
-        }
-    },
-    419495936: {
-        '*': {
-            '/axapi/v3/logoff': None,
-            '*': AE::InvalidSessionID
-        }
-    },
-    520749062: {
-        '*': {
-            '*': AE::NotFound
-        }
-    },
-    654311495: {
-        '*': {
-            '*': AE::Exists
-        }
-    },
-    67240011: {
-        '*': {
-            '*': AE::Exists
-        }
-    },
-    754974732: {
-        '*': {
-            '*': AE::Exists
-        }
-    },
-    754974733: {
-        '*': {
-            '*': AE::PartitionIdExists
-        }
-    },
-    1023410176: {
-        'DELETE': {
-            '*': None
+module ErrorResp
+
+    @@RESPONSE_CODES = {
+        33619969: {
+            '*': {
+                '*': AE::InUse
+            }
         },
-        '*': {
-            '*': AE::NotFound
-        }
-    },
-    1023410181: {
-        'DELETE': {
-            '*': None
+        67371011: {
+            '*': {
+                '*': AE::Exists
+             }
         },
-        '*': {
-            '/axapi/v3/slb/service-group/.*/member/': AE::NotFound,
-            '*': AE::NotFound
-        }
-    },
-    1023410183: {
-        '*': {
-            '*': AE::Exists
-        }
-    },
-    1023451145: {
-        '*': {
-            '*': AE::Exists
-        }
-    },
-    1023459339: {
-        '*': {
-            '/axapi/v3/slb/server': AE::Exists
-        }
-    },
-    1023459393: {
-        '*': {
-            '*': AE::InvalidParameter
-        }
-    },
-    1023459335: {
-        '*': {
-            '*': AE::Exists
-        }
-    },
-    1023460352: {
-        'DELETE': {
-            '*': None
+        419495936: {
+            '*': {
+                '/axapi/v3/logoff': None,
+                '*': AE::InvalidSessionID
+            }
         },
-        '*': {
-            '*': AE::NotFound
-        }
-    },
-    1023463424: {
-        '*': {
-            '*': AE::ConfigManagerNotReady
-        }
-    },
-    1023475722: {
-        '*': {
-            '*': AE::NotFound
-        }
-    },
-    1023508480: {
-        '*': {
-            '*': AE::AxapiJsonFormatError
-        }
-    },
-    1023509504: {
-        '*': {
-            '*': AE::NotFound
-        }
-    },
-    1023524874: {
-        '*': {
-            '*': AE::AxapiJsonFormatError
-        }
-    },
-    1023656960: {
-        '*': {
-            '*': AE::NotFound
-        }
-    },
-    1023656962: {
-        '*': {
-            '*': AE::NotFound
-        }
-    },
-    1207960052: {
-        '*': {
-            '/axapi/v3/logoff': None,
-            '*': AE::InvalidSessionID
-        }
-    },
-    1207959957: {
-        '*': {
-            '*': AE::NotFound
-        }
-    },
-    1208025092: {
-        '*': {
-            '/axapi/v3/logoff': None,
-            '*': AE::InvalidSessionID
-        }
-    },
-    1208025095: {
-        '*': {
-            '*': AE::ConfigManagerNotReady
-        }
-    },
-    1023443968: {
-        'DELETE': {
-            '*': None
+        520749062: {
+            '*': {
+                '*': AE::NotFound
+            }
         },
-        '*': {
-            '*': AE::NotFound
-        }
-    },
-    1023451144: {
-        '*': {
-            '*': AE::Exists
-        }
-    },
-    1023475727: {
-        '*': {
-            '*': AE::NotFound
-        }
-    },
-    4294967295: {
-        '*': {
-            '*': AE::ConfigManagerNotReady
-        }
-    },
-}
+        654311495: {
+            '*': {
+                '*': AE::Exists
+            }
+        },
+        67240011: {
+            '*': {
+                '*': AE::Exists
+            }
+        },
+        754974732: {
+            '*': {
+                '*': AE::Exists
+            }
+        },
+        754974733: {
+            '*': {
+                '*': AE::PartitionIdExists
+            }
+        },
+        1023410176: {
+            'DELETE': {
+                '*': None
+            },
+            '*': {
+                '*': AE::NotFound
+            }
+        },
+        1023410181: {
+            'DELETE': {
+                '*': None
+            },
+            '*': {
+                '/axapi/v3/slb/service-group/.*/member/': AE::NotFound,
+                '*': AE::NotFound
+            }
+        },
+        1023410183: {
+            '*': {
+                '*': AE::Exists
+            }
+        },
+        1023451145: {
+            '*': {
+                '*': AE::Exists
+            }
+        },
+        1023459339: {
+            '*': {
+                '/axapi/v3/slb/server': AE::Exists
+            }
+        },
+        1023459393: {
+            '*': {
+                '*': AE::InvalidParameter
+            }
+        },
+        1023459335: {
+            '*': {
+                '*': AE::Exists
+            }
+        },
+        1023460352: {
+            'DELETE': {
+                '*': None
+            },
+            '*': {
+                '*': AE::NotFound
+            }
+        },
+        1023463424: {
+            '*': {
+                '*': AE::ConfigManagerNotReady
+            }
+        },
+        1023475722: {
+            '*': {
+                '*': AE::NotFound
+            }
+        },
+        1023508480: {
+            '*': {
+                '*': AE::AxapiJsonFormatError
+            }
+        },
+        1023509504: {
+            '*': {
+                '*': AE::NotFound
+            }
+        },
+        1023524874: {
+            '*': {
+                '*': AE::AxapiJsonFormatError
+            }
+        },
+        1023656960: {
+            '*': {
+                '*': AE::NotFound
+            }
+        },
+        1023656962: {
+            '*': {
+                '*': AE::NotFound
+            }
+        },
+        1207960052: {
+            '*': {
+                '/axapi/v3/logoff': None,
+                '*': AE::InvalidSessionID
+            }
+        },
+        1207959957: {
+            '*': {
+                '*': AE::NotFound
+            }
+        },
+        1208025092: {
+            '*': {
+                '/axapi/v3/logoff': None,
+                '*': AE::InvalidSessionID
+            }
+        },
+        1208025095: {
+            '*': {
+                '*': AE::ConfigManagerNotReady
+            }
+        },
+        1023443968: {
+            'DELETE': {
+                '*': None
+            },
+            '*': {
+                '*': AE::NotFound
+            }
+        },
+        1023451144: {
+            '*': {
+                '*': AE::Exists
+            }
+        },
+        1023475727: {
+            '*': {
+                '*': AE::NotFound
+            }
+        },
+        4294967295: {
+            '*': {
+                '*': AE::ConfigManagerNotReady
+            }
+        },
+    }
+
+    def self.raise_axapi_error(response, method, api_url, headers)
+        if response['authorizationschema']
+            code = response['authorizationschema']['code']
+            s = response['authorzationschema']['error']
+            if code == 401
+                if headers and 'Authorization' in headers
+                    raise AE::InvalideSessionID(code, s)
+                else
+                    raise AE::AuthenticationFailure(code, s)
+                end
+            elsif code == 403
+                raise AE::Authenticationfailure(code, s)
+            end
+        end
+    end
+
+    def self.raise_axapi_ex(response, method, api_url)
+        if response['response']['err']['code']
+            if RESPONSE_CODES[code]
+                ex_dict = RESPONSE_CODES[code]
+                ex = nil
+
+                if ex_dict[method]
+                    x = ex_dict[method]
+                else
+                    x = ex_dict['*']
+                end
+
+                matched = False
+                x.each do |k, v|
+                    if k != '*' && 
+                end
+            end
+        end
+    end
+end
