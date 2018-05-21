1b687f85352a0c2eecea214079ffc8cda229aa0f
Hunter Thompson <thompson.grey.hunter@gmail.com>
2018-05-21 22:11:51 +0000

    Removed test client

--- a10client.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'axapi_http'
-
-client = A10Client::client_factory('10.48.6.222', 'https', 443, 'admin', 'a10')
-
-client.post("/axapi/v3/slb/virtual-server/slb4", params: {"ip_address" => "1.1.1.1"})
